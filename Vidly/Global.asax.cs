b665fb1d63cd5916b20e4f08229955c3356d9422
Nevinson <nevinson23@gmail.com>
2019-03-30 01:06:57 +0200

    Add Automapper Mapping Profile.

--- Vidly/Global.asax.cs
+++ Vidly/Global.asax.cs
@@ -1,11 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using AutoMapper;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Vidly.App_Start;
 
 namespace Vidly
 {
@@ -13,8 +11,9 @@ namespace Vidly
     {
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
+            Mapper.Initialize(c => c.AddProfile<MappingProfile>());
             GlobalConfiguration.Configure(WebApiConfig.Register);
+            AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
